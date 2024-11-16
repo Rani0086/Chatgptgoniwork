b7f6520701d2e59af81d720f3342562dd674fc5b
Rani0086 <shantidhaakad@gmail.com>
2024-11-16 10:24:17 +0530

    Update Shayri.py

--- GONI/modules/Shayri.py
+++ GONI/modules/Shayri.py
@@ -73,10 +73,10 @@ async def shayri(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/TG_FRIENDSS"
+                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/Planetsadala"
                     ),
                     InlineKeyboardButton(
-                        "✨𝙾𝙵𝙵𝙸𝙲𝙴✨", url=f"https://t.me/VIP_CREATORS"
+                        "✨𝙾𝙵𝙵𝙸𝙲𝙴✨", url=f"https://t.me/lolpagalokigc"
                     ),
                 ]
             ]
@@ -109,7 +109,7 @@ async def send_good_night():
             shayari = random.choice(night_shayari)
             await GONI.send_photo(
                 chat_id,
-                photo="https://telegra.ph//file/06649d4d0bbf4285238ee.jpg",
+                photo="https://files.catbox.moe/iz3d3a.jpg",
                 caption=f"**{shayari}**",
                 reply_markup=add_buttons,
             )
@@ -128,7 +128,7 @@ async def send_good_morning():
             shayari = random.choice(morning_shayari)
             await GONI.send_photo(
                 chat_id,
-                photo="https://telegra.ph//file/14ec9c3ff42b59867040a.jpg",
+                photo="https://files.catbox.moe/n4qv2v.jpg",
                 caption=f"**{shayari}**",
                 reply_markup=add_buttons,
             )
